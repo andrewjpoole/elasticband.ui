5dbacc55e8e75a95c1d30599f6eaaefe09644d18
andrewjpoole <andrewjpoole@gmail.com>
2020-03-01 20:47:18 +0000

    Added a seperate component for new collections

--- wwwroot/js/monacoEditor.js
+++ wwwroot/js/monacoEditor.js
@@ -3,6 +3,7 @@
 		try {
 			console.log('loading monaco editor from cdn...');
 			require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.17.1/min/vs' } });
+						
 			window.MonacoEnvironment = { getWorkerUrl: () => proxy };
 
 			let proxy = URL.createObjectURL(new Blob([`
@@ -11,15 +12,6 @@
 			};
 			importScripts('https://unpkg.com/monaco-editor@0.17.1/min/vs/base/worker/workerMain.js');
 			`], { type: 'text/javascript' }));
-
-			require(["vs/editor/editor.main"], function () {				
-				var collectionModelUri = monaco.Uri.parse("a://" + id + ".json");
-				window.collectionModel = monaco.editor.createModel("{}", "json", collectionModelUri);
-
-				var itemModelUri = monaco.Uri.parse("a://" + id + ".json");
-				window.itemModel = monaco.editor.createModel("{}", "json", itemModelUri);				
-			});
-
 		}
 		catch (e) {
 			console.error('error loading monaco editor', e);
@@ -36,30 +28,7 @@
 				return;
 			}
 
-			//if (window[elementId] != undefined) {
-			//	console.log('monaco editor setContents - updating existing editor instance');
-			//	window[elementId].value = contents;
-			//	window[elementId].language = lang;
-			//	window[elementId].theme = 'vs-dark';
-			//	return;
-			//}
-
-			//if (window[elementId] != undefined) {
-			//	console.log('monaco editor setContents - nulling old editor instance');
-			//	//window[elementId].dispose();
-			//	window[elementId] = null;
-			//}
-
 			require(["vs/editor/editor.main"], function () {
-				//var modelUri = monaco.Uri.parse("a://" + id + ".json");
-				//var model = monaco.editor.createModel(contents, "json", modelUri);
-
-				//window.editor = monaco.editor.create(editorElement, {
-				//	model: model,
-				//	language: lang,
-				//	theme: 'vs-dark'
-				//});
-
 				window[elementId] = monaco.editor.create(editorElement, {
 					value: contents,
 					language: lang,
